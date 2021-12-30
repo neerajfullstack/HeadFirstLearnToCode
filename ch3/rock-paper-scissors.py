3d7637bcdb65ccab71a5461b397cfd1027489cd3
Neeraj Gupta <neerajfullstack@gmail.com>
2021-12-31 02:25:06 +0530

    added documentation to rock.py

--- ch3/rock-paper-scissors.py
+++ ch3/rock-paper-scissors.py
@@ -1,14 +1,23 @@
+# here we are doing some setup by importing the random module
+# and setting up the winner variable
+
 import random
 
 winner = ''
+
+# The computer randomly chooses one item from the list below
 
 choices = ['rock', 'paper', 'scissors']
 
 computer_choice = random.choice(choices)
 
+# get the user's choice with a simple input statement
+
 user_choice = input('rock, paper or scissors?')
 
-# Evaluate the results
+# Here's our game logic which checks to see if the computer wins or not
+# and makes the appropriate change to the winner var
+
 if user_choice == computer_choice:
     winner = 'Tie'
 elif computer_choice == 'paper' and user_choice == 'rock':
@@ -20,7 +29,8 @@ elif computer_choice == 'scissors' and user_choice == 'paper':
 else:
     winner = 'User'
 
-# Display the winner
+# Here we announce the game was a tie or winner along with the
+# computer's choice
 
 if winner == 'Tie':
     print('We both chose', computer_choice, 'play again.')
