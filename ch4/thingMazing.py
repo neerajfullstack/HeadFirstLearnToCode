177185b952612c2fb56374c2c4ec88aeca6e1f93
Neeraj Gupta <neerajfullstack@gmail.com>
2022-01-01 03:52:20 +0530

    ThingAMazing.py

---
+++ ch4/thingMazing.py
@@ -0,0 +1,18 @@
+chars = ['t', 'a', 'c', 'o']
+
+output = ''
+length = len(chars)
+i = 0
+while (i < length):
+	output = output + chars[i]
+	i = i + 1
+
+length = length * -1
+
+i = -2
+
+while(i >= length):
+	output = output + chars[i]
+	i = i - 1
+
+print(output)
