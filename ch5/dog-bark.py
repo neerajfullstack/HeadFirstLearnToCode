c63b2c5c8ae65fd35366d90d835737b79492579d
Neeraj Gupta <neerajfullstack@gmail.com>
2022-01-04 03:54:54 +0530

    Car or plane

---
+++ ch5/dog-bark.py
@@ -0,0 +1,17 @@
+print("Get the dogs ready")
+
+def bark(name, weight):
+    if weight > 20:
+        print(name, 'Say WOOF WOOF')
+    else:
+        print(name, 'say woof woof')
+
+def sayHello(name):
+    print('Hello', name)
+
+
+sayHello('Neeraj')
+
+bark('Codie', 40)
+
+print("Okay, we're all done")
