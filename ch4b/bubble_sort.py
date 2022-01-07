c68c6527af1bd2b4d5ac236fe2f15c7b6e40f675
Neeraj Gupta <neerajfullstack@gmail.com>
2022-01-07 07:19:37 +0530

    bubble sort done

--- ch4b/bubble_sort.py
+++ ch4b/bubble_sort.py
@@ -1,4 +1,4 @@
-def bubble_sort(sortable):
+def bubble_sort(sortable, numbers):
     swapped = True
 
     while swapped:
@@ -8,11 +8,15 @@ def bubble_sort(sortable):
                 temp = sortable[i]
                 sortable[i] = sortable[i+1]
                 sortable[i+1] = temp
+                temp = numbers[i]
+                numbers[i] = numbers[i+1]
+                numbers[i+1] = temp
                 swapped = True
 
 scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44]
 
 
-
-bubble_sort(scores)
-print(scores)
+number_of_scores = len(scores)
+solution_numbers = list(range(number_of_scores))
+bubble_sort(scores, solution_numbers)
+print(scores, solution_numbers)
